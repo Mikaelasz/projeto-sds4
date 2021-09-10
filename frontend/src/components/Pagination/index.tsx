250b3f7fdc8c01c549c3c7502c20dc12a2ffbe1f
Mikaela Soares <miih.arj@gmail.com>
2021-09-10 16:28:13 -0300

    Env var

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -12,7 +12,7 @@ const Pagination = ( { page, onPageChance } : Props) => {
     return (
         <div className="row d-flex justify-content-center">
             <nav>
-                <ul className="pagination">
+                <ul className="pagination justify-content-center">
                     <li className={`page-item ${page.first ?  'disabled' : ''}`}>
                         <button className="page-link" onClick={() => onPageChance(page.number - 1)}>Anterior</button>
                     </li>
